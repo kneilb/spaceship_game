f22b6f5d3f995cc9f268c0b63ceddbcf2ffd3168
Neil Burningham <neil.burningham@mediakind.com>
2023-11-19 20:48:13 +0000

    Episode 2 code

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,25 @@
+mod camera;
+mod debug;
+mod movement;
+mod spaceship;
+
 use bevy::prelude::*;
-
-#[derive(Component, Debug)]
-struct Position {
-    x: f32,
-    y: f32,
-}
-
-#[derive(Component, Debug)]
-struct Velocity {
-    x: f32,
-    y: f32,
-}
-
-fn spawn_spaceship(mut commands: Commands) {
-    commands.spawn((Position { x: 0.0, y: 0.0 }, Velocity {x: 1.0, y: 1.0}));
-}
-
-fn update_position(mut query: Query<(&Velocity, &mut Position)>) {
-    for (velocity, mut position) in query.iter_mut() {
-        position.x += velocity.x;
-        position.y += velocity.y;
-    }
-}
-
-fn print_position(query: Query<(Entity, &Position)>) {
-    for (entity, position) in query.iter() {
-        info!("Entity {:?} is at position {:?}", entity, position);
-    }
-}
 
 fn main() {
     println!("Hurrow, Bevy!");
 
     App::new()
-    .add_systems(Startup, spawn_spaceship)
-    .add_systems(Update, (update_position, print_position))
-    .add_plugins(DefaultPlugins)
-    .run();
+        // Bevy built-ins
+        .insert_resource(ClearColor(Color::rgb(0.1, 0.0, 0.1)))
+        .insert_resource(AmbientLight {
+            color: Color::default(),
+            brightness: 0.75,
+        })
+        .add_plugins(DefaultPlugins)
+        // Game plugins.
+        .add_plugins(spaceship::SpaceshipPlugin)
+        .add_plugins(camera::CameraPlugin)
+        .add_plugins(movement::MovementPlugin)
+        .add_plugins(debug::DebugPlugin)
+        .run();
 }

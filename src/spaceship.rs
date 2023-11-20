cb13d2081bf6a551869abd7baf81b557a78cc418
Neil Burningham <neil.burningham@mediakind.com>
2023-11-20 13:59:05 +0000

    Fire zee missiles!

--- src/spaceship.rs
+++ src/spaceship.rs
@@ -9,16 +9,23 @@ const STARTING_TRANSLATION: Vec3 = Vec3::new(0.0, 0.0, -20.0);
 const SPACESHIP_SPEED: f32 = 25.0;
 const SPACESHIP_ROTATION_SPEED: f32 = 2.5;
 const SPACESHIP_ROLL_SPEED: f32 = 2.5;
+const MISSILE_SPEED: f32 = 50.0;
+const MISSILE_FORWARD_SPAWN_SCALAR: f32 = 7.5;
 
 #[derive(Component, Debug)]
 pub struct Spaceship;
+
+#[derive(Component, Debug)]
+struct SpaceshipMissile;
 
 pub struct SpaceshipPlugin;
 
 impl Plugin for SpaceshipPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(PostStartup, spawn_spaceship)
-            .add_systems(Update, spaceship_movement_controls);
+        app.add_systems(PostStartup, spawn_spaceship).add_systems(
+            Update,
+            (spaceship_movement_controls, spaceship_weapon_controls),
+        );
     }
 }
 
@@ -73,8 +80,36 @@ fn spaceship_movement_controls(
     // NB. We have to negate this because the models & Bevy disagree on which way to face in Z!
     velocity.value = -transform.forward() * movement;
 
+    // Rotate the spaceship around the Y-axis.
+    // Ignores the Z-axis rotation applied below.
     transform.rotate_y(rotation);
 
+    // Rotate the spaceship around the local Z-axis.
+    // This occurs relative to the current rotation.
     transform.rotate_local_z(roll);
+}
 
+fn spaceship_weapon_controls(
+    mut commands: Commands,
+    query: Query<&Transform, With<Spaceship>>,
+    keyboard_input: Res<Input<KeyCode>>,
+    scene_assets: Res<SceneAssets>,
+) {
+    let transform = query.single();
+    if keyboard_input.pressed(KeyCode::Space) {
+        commands.spawn((
+            MovingObjectBundle {
+                velocity: Velocity::new(-transform.forward() * MISSILE_SPEED),
+                acceleration: Acceleration::new(Vec3::ZERO),
+                model: SceneBundle {
+                    scene: scene_assets.missile.clone(),
+                    transform: Transform::from_translation(
+                        transform.translation + -transform.forward() * MISSILE_FORWARD_SPAWN_SCALAR,
+                    ),
+                    ..default()
+                },
+            },
+            SpaceshipMissile,
+        ));
+    }
 }

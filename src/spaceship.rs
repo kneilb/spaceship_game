f204aef14ef975f0b0d783f30853d58d49d77a09
Neil Burningham <neil.burningham@mediakind.com>
2023-11-20 13:31:14 +0000

    Spaceship moves back & forward with W/S

--- src/spaceship.rs
+++ src/spaceship.rs
@@ -6,24 +6,62 @@ use crate::{
 };
 
 const STARTING_TRANSLATION: Vec3 = Vec3::new(0.0, 0.0, -20.0);
-const STARTING_VELOCITY: Vec3 = Vec3::new(0.0, 0.0, 1.0);
+const SPACESHIP_SPEED: f32 = 25.0;
+const SPACESHIP_ROTATION_SPEED: f32 = 2.5;
+const SPACESHIP_ROLL_SPEED: f32 = 2.5;
+
+#[derive(Component, Debug)]
+pub struct Spaceship;
 
 pub struct SpaceshipPlugin;
 
 impl Plugin for SpaceshipPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(PostStartup, spawn_spaceship);
+        app.add_systems(PostStartup, spawn_spaceship)
+            .add_systems(Update, spaceship_movement_controls);
     }
 }
 
 fn spawn_spaceship(mut commands: Commands, scene_assets: Res<SceneAssets>) {
-    commands.spawn(MovingObjectBundle {
-        velocity: Velocity::new(STARTING_VELOCITY),
-        acceleration: Acceleration::new(Vec3::ZERO),
-        model: SceneBundle {
-            scene: scene_assets.spaceship.clone(),
-            transform: Transform::from_translation(STARTING_TRANSLATION),
-            ..default()
+    commands.spawn((
+        MovingObjectBundle {
+            velocity: Velocity::new(Vec3::ZERO),
+            acceleration: Acceleration::new(Vec3::ZERO),
+            model: SceneBundle {
+                scene: scene_assets.spaceship.clone(),
+                transform: Transform::from_translation(STARTING_TRANSLATION),
+                ..default()
+            },
         },
-    });
+        Spaceship,
+    ));
+}
+
+fn spaceship_movement_controls(
+    // With<Spaceship> ensures that we only get spaceships - and don't move asteroids with the keys...!
+    mut query: Query<(&mut Transform, &mut Velocity), With<Spaceship>>,
+    keyboard_input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    let (mut transform, mut velocity) = query.single_mut();
+
+    let mut rotation = 0.0;
+    let mut roll = 0.0;
+    let mut movement = 0.0;
+
+    if keyboard_input.pressed(KeyCode::W) {
+        movement = SPACESHIP_SPEED
+    } else if keyboard_input.pressed(KeyCode::S) {
+        movement = -SPACESHIP_SPEED;
+    }
+
+    // Update the spaceship's velocity based on new direction
+    // NB. We have to negate this because the models & Bevy disagree on which way to face in Z!
+    velocity.value = -transform.forward() * movement;
+
+    if keyboard_input.pressed(KeyCode::A) {
+        rotation = SPACESHIP_ROTATION_SPEED
+    } else if keyboard_input.pressed(KeyCode::D) {
+        rotation = SPACESHIP_ROTATION_SPEED
+    }
 }

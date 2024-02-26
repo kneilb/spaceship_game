6a301524029781b8e348f1fbc76a528366791c76
Neil Burningham <neil.burningham@mediakind.com>
2024-02-26 21:54:20 +0000

    Add prints to windows.sh

--- windows.sh
+++ windows.sh
@@ -5,9 +5,15 @@ set -o errexit
 readonly target=x86_64-pc-windows-gnu
 readonly game=spaceship_game
 
+echo "Building..."
+
 # cargo build --target $target
 cross build --target $target
 
+echo "Copying..."
+
 cp target/$target/debug/$game.exe .
+
+echo "Running..."
 
 exec ./$game.exe "$@"
